53f68b48432b950641ba36780bd7e489ace1428f
Ning <sszhangningwowow@163.com>
2022-07-17 09:06:42 +0800

    项目初始创建

---
+++ gulimail-product/src/main/java/vom/atguigu/gulimall/product/GulimailProductApplication.java
@@ -0,0 +1,13 @@
+package vom.atguigu.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailProductApplication.class, args);
+    }
+
+}
---
+++ gulimail-product/src/test/java/vom/atguigu/gulimall/product/GulimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package vom.atguigu.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
