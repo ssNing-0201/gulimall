4e0dd33c8828914272ba25aa89eb5d7508de2c0e
Ning <sszhangningwowow@163.com>
2022-07-18 22:39:01 +0800

    1、项目初始创建 2、项目后端使用人人开源进行快速开发 3、所有服务引入Nacos注册中心，注册成功。   coupon服务和member使用openFeign远程调用服务。

--- gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimailProductApplication {
 
     public static void main(String[] args) {
