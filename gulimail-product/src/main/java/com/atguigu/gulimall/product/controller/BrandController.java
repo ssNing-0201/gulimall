165516273ff5b8f4c7463b093d1053ca2de57df6
Ning <sszhangningwowow@163.com>
2022-08-15 11:33:44 +0800

    完成了一个自定义校验，并开始了属性分组功能。 1、基本页面搭建完成。

--- gulimail-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import com.atguigu.common.valid.AddGroup;
 import com.atguigu.common.valid.UpdateGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -90,6 +91,17 @@ public class BrandController {
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
     public R update(@Validated(UpdateGroup.class) @RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改状态
+     */
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated({UpdateStatusGroup.class}) @RequestBody BrandEntity brand) {
         brandService.updateById(brand);
 
         return R.ok();
--- gulimail-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/entity/BrandEntity.java
@@ -1,10 +1,14 @@
 package com.atguigu.gulimall.product.entity;
 
 import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.ListValue;
 import com.atguigu.common.valid.UpdateGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
 import java.io.Serializable;
+
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
@@ -13,7 +17,7 @@ import javax.validation.constraints.*;
 
 /**
  * Ʒ?
- * 
+ *
  * @author Ning
  * @email sszhangningwowow@gmail.com
  * @date 2022-07-18 09:56:06
@@ -21,46 +25,48 @@ import javax.validation.constraints.*;
 @Data
 @TableName("pms_brand")
 public class BrandEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 品牌id
-	 */
-	@NotNull(message = "品牌ID不能为空",groups = {UpdateGroup.class})
-	@Null(message = "新增不能指定ID",groups = {AddGroup.class})
-	@TableId
-	private Long brandId;
-	/**
-	 * 品牌名
-	 */
-	@NotBlank(message = "品牌名不能为空，必须提交",groups = {UpdateGroup.class,AddGroup.class})
-	private String name;
-	/**
-	 * 品牌logo地址
-	 */
-	@NotEmpty(message = "logo地址不能为空",groups = {AddGroup.class})
-	@URL(message = "logo必须是一个合法的url地址",groups = {UpdateGroup.class,AddGroup.class})
-	private String logo;
-	/**
-	 * 介绍
-	 */
-	private String descript;
-	/**
-	 * 显示状态[0-不显示;1-显示]
-	 */
-	private Integer showStatus;
-	/**
-	 * 检索首字母
-	 */
-	@NotEmpty(message = "检索首字母地址不能为空",groups = {AddGroup.class})
-	@Pattern(regexp="/^[a-zA-Z]$/",message = "检索首字母必须是一个字母" ,groups = {UpdateGroup.class,AddGroup.class})
-	private String firstLetter;
-	/**
-	 * 排序
-	 */
-	@NotNull(message = "排序地址不能为空",groups = {AddGroup.class})
-	@Min(value = 0,message = "排序必须大于等于0",groups = {UpdateGroup.class,AddGroup.class})
-	private Integer sort;
+    /**
+     * 品牌id
+     */
+    @NotNull(message = "品牌ID不能为空", groups = {UpdateGroup.class, UpdateStatusGroup.class})
+    @Null(message = "新增不能指定ID", groups = {AddGroup.class})
+    @TableId
+    private Long brandId;
+    /**
+     * 品牌名
+     */
+    @NotBlank(message = "品牌名不能为空，必须提交", groups = {UpdateGroup.class, AddGroup.class})
+    private String name;
+    /**
+     * 品牌logo地址
+     */
+    @NotEmpty(message = "logo地址不能为空", groups = {AddGroup.class})
+    @URL(message = "logo必须是一个合法的url地址", groups = {UpdateGroup.class, AddGroup.class})
+    private String logo;
+    /**
+     * 介绍
+     */
+    private String descript;
+    /**
+     * 显示状态[0-不显示;1-显示]
+     */
+    @NotNull(message = "显示状态不能为空",groups = {UpdateStatusGroup.class})
+    @ListValue(vals = {0, 1}, groups = {AddGroup.class, UpdateStatusGroup.class})
+    private Integer showStatus;
+    /**
+     * 检索首字母
+     */
+    @NotEmpty(message = "检索首字母地址不能为空", groups = {AddGroup.class})
+    @Pattern(regexp = "^[a-zA-Z]$", message = "检索首字母必须是一个字母", groups = {UpdateGroup.class, AddGroup.class})
+    private String firstLetter;
+    /**
+     * 排序
+     */
+    @NotNull(message = "排序地址不能为空", groups = {AddGroup.class})
+    @Min(value = 0, message = "排序必须大于等于0", groups = {UpdateGroup.class, AddGroup.class})
+    private Integer sort;
 
     public BrandEntity() {
 
